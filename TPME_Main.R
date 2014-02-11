5c1dd19fc3e03801478107ff652e8ea74803a003
Matthew Denny <matthewjdenny@gmail.com>
2014-02-11 17:41:23 -0500

    Adding main script to call function

---
+++ TPME_Main.R
@@ -0,0 +1,15 @@
+# Main class to run TPME Model
+rm(list=ls())
+
+# 1. Load packages
+library(stringr)
+library(statnet)
+library(Rcpp)
+
+# 2. Load user defined functions
+source("TPME_Run_Analysis.R")
+
+# 3. Load data
+# 4. Run analysis
+# 5. Output and Analyze results
+
