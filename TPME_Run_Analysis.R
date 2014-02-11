97020dbba59fab562ab3d99a4c6b4f1ef54359be
Matthew Denny <matthewjdenny@gmail.com>
2014-02-11 17:44:35 -0500

    Script that is called to run the algorithm

---
+++ TPME_Run_Analysis.R
@@ -0,0 +1,43 @@
+# Defines function which runs main analysis
+
+Run_Analysis <- function(NumberOfIterations = 1000, Alpha =1, Beta = 0.01, NumberOfTopics = 50, Authors, EdgeMatrix ,DocWordMatrix, Vocab){
+    
+    #Initialize variables (topic state, word state, edge state)
+    NUMBER_OF_DOCUMENTS # the number of emails
+    
+    #1. Sample token topic assignments
+    for(d in 1:NUMBER_OF_DOCUMENTS){
+        
+        
+    }
+    
+    #2. Sample document edge assingments 
+    for(d in 1:NUMBER_OF_DOCUMENTS){
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+} # End of Run_Analysis definition
+
+# test
