9838ff7c5c86c47f2f6b03160679007a336741dc
Matthew Denny <matthewjdenny@gmail.com>
2014-02-11 22:26:55 -0500

    Added the framework for token topic sampler.

--- TPME_Run_Analysis.R
+++ TPME_Run_Analysis.R
@@ -1,16 +1,51 @@
 # Defines function which runs main analysis
+# gloabl variables have first letter of every word capitalized
+# local variables are not capitalized
+# user defined functions are in all caps
 
-Run_Analysis <- function(Number_Of_Iterations = 1000, Alpha =1, Beta = 0.01, Number_Of_Topics = 50, Author_Attributes, Document_Edge_Matrix ,Document_Word_Matrix, Vocabulary, Latent_Dimensions, Save_Interval){
+Run_Analysis <- function(Number_Of_Iterations = 1000, Base_Alpha =1, Base_Beta = 0.01, Number_Of_Topics = 50, Author_Attributes, Document_Edge_Matrix ,Document_Word_Matrix, Vocabulary, Latent_Dimensions){
     
-    #==== Initialize all variables, latent spaces edge assingments and topic assignments ====#
+    #================ set working driectory and source all functions ====================#
+    setwd("~/Dropbox/PINLab/Projects/R_Code/TPMNE")
+    source("TPME_Sample_Token_Topic_Assignments.R")
+    
+    #================= Initialize all variables, latent spaces edge assingments and topic assignments ==============#
     
     Latent_Space_Intercepts <- rep(10, Number_Of_Topics) #this is set to 10 becasue it can only get smaller
-    NUMBER_OF_DOCUMENTS <- length(Document_Word_Matrix[,1] # the number of documents is equal to the number of rows 
+    
+    Number_Of_Documents <- length(Document_Word_Matrix[,1]) # the number of documents is equal to the number of rows 
+                                  
     Metropolis_Hastings_Control_Parameter <- 0 #this is used to shrink the proposal variace of the metropolis hastings portion of the algorithm 
     
+    Number_Of_Authors <- length(Author_Attributes[,1]) 
+    
+    Beta <- Base_Beta*length(Vocabulary)
+    
+    #we define alpha to be a vector so that it can accomodate an asymmetric base measure in the future
+    Alpha_Base_Measure_Vector <- rep((Base_Alpha/Number_Of_Topics,Number_Of_Topics)
+    
+    #token topic assignemnts are stores in a list of vectors data structure
+    Token_Topic_Assignments <- list()
+    for(d in 1:Number_Of_Documents){
+        #allocate a vector of zeros equal to the number of tokens in the document
+        #cur_token_assignments <- rep(0,sum(Document_Word_Matrix[d,])) #assign a zero vector of topic assignments if we then wanted to do a more complicated sampling proceedure 
+        cur_token_assignments <- sample(1:Number_Of_Topics,sum(Document_Word_Matrix[d,]),replace= T) #samples from a discrete uniform distribution
+        Token_Topic_Assignments <- append(Token_Topic_Assignments,list(cur_token_assignments))
+    }
+    
+    #sample latent space postions for each actor for each topic from a uniform distribution. This will be a list of matricies data structure with rows in each matrix being the latent dimensions and columns being each topic 
+    Latent_Space_Positions <- list()
+    for(a in 1:Number_Of_Authors){ 
+        latent_space_positions <- matrix(0,nrow = Latent_Dimensions, ncol = Number_Of_Topics)
+        for(s in 1:Latent_Dimensions){
+            latent_space_positions[s,] <- runif(Number_Of_Topics, min = -1, max = 1) #samples from a continuous uniform distribution on (-1,1)
+        }
+        #now add to the list object
+        Latent_Space_Positions <- append(Latent_Space_Positions,list(latent_space_positions))
+    }
     
                                   
-    #===== MAIN LOOP OVER NUMER OF ITTERATIONS =====#                              
+    #==================== MAIN LOOP OVER NUMER OF ITTERATIONS ====================#                              
     for(i in 1:Number_Of_Iterations){
         
         #1. Set proposal variance for current itteration for metropolis hastings step
@@ -21,9 +56,9 @@ Run_Analysis <- function(Number_Of_Iterations = 1000, Alpha =1, Beta = 0.01, Num
         
         
         #2. Sample token topic assignments
-        for(d in 1:NUMBER_OF_DOCUMENTS){
+        for(d in 1:Number_Of_Documents){
             if(sum(Document_Word_Matrix[d,]) > 0){ #if there is atleast one token in the document
-                
+                Token_Topic_Assignments[[d]][[1]] <- SAMPLE_TOKEN_TOPIC_ASSIGNMENTS(Token_Topic_Assignments[[d]][[1]],Number_Of_Topics,Number_Of_Authors,Alpha_Base_Measure_Vector, Beta)
             }else{ #assign the docuemnt a dummy word and dummy topic
                 
             }
@@ -32,14 +67,14 @@ Run_Analysis <- function(Number_Of_Iterations = 1000, Alpha =1, Beta = 0.01, Num
         }
         
         #3. Sample document edge assingments 
-        for(d in 1:NUMBER_OF_DOCUMENTS){
+        for(d in 1:Number_Of_Documents){
             
             
         }
         
         #4. Sample latent positions for each actor and topic
         for(t in 1:Number_Of_Topics){
-            for(a in length(Author_Attributes[,1])){ #Author_Attributes is a matrix with one row per author and one column per attribute
+            for(a in 1:Number_Of_Authors){ #Author_Attributes is a matrix with one row per author and one column per attribute
                 
                 
             }
@@ -50,7 +85,7 @@ Run_Analysis <- function(Number_Of_Iterations = 1000, Alpha =1, Beta = 0.01, Num
             
         }
         
-        #6. If we are at a save interval, save everything to an .RData object
+        
         
         
     }#end of main loop over number of itterations                     
---
+++ TPME_Sample_Token_Topic_Assignments.R
@@ -0,0 +1,13 @@
+#this script defines the SAMPLE_TOKEN_TOPIC_ASSIGNMENTS function
+
+
+SAMPLE_TOKEN_TOPIC_ASSIGNMENTS <- function(document_token_topic_assignments,number_of_topics,number_of_authors,alpha_base_measure_vector, beta){
+    
+    new_token_topic_assignments <- rep(0,length(document_token_topic_assignments))
+    
+    
+    
+    
+    
+    return(new_token_topic_assignments)
+}
