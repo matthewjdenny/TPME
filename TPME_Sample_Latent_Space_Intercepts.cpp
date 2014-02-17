f68e89a1721990f1b297f9777b071c9e4c9dcbf1
Matthew Denny <matthewjdenny@gmail.com>
2014-02-16 23:53:05 -0500

    Started filling out c++ function to sample latent space intercepts

---
+++ TPME_Sample_Latent_Space_Intercepts.cpp
@@ -0,0 +1,38 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// Below is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar)
+
+// For more on using Rcpp click the Help button on the editor toolbar
+
+// [[Rcpp::export]]
+NumericVector SAMPLE_NEW_TOPIC_INTERCEPT_CPP(int topic,int latent_dimensions,int number_of_actors, int proposal_variance){
+    
+    Function get_token_topic_assignment("get_token_topic_assignment");
+    Function get_observed_edge_value("get_observed_edge_value");
+    Function log_multinomial_draw("log_multinomial_draw");
+    Function Log_Probability_Of_Edge("Log_Probability_Of_Edge");
+    int document_author = author - 1;
+    
+    NumericVector edge_topic_assignments(number_of_actors);
+     
+    
+    for(int a = 0; a < number_of_actors; ++a){
+        if(document_author != a){
+            int recipient = a + 1;
+            NumericVector edge_log_probabilities(number_of_tokens);
+            for(int w = 0; w < number_of_tokens; ++w){
+                int token = w + 1;
+                int topic_assignment = as<int>(get_token_topic_assignment(document,token));
+                int actual_edge = as<int>(get_observed_edge_value(document,recipient));
+                edge_log_probabilities[w] = as<double>(Log_Probability_Of_Edge(topic_assignment,author,recipient,actual_edge,0,0));  
+            }
+            int sampled_token = as<int>(log_multinomial_draw(edge_log_probabilities));
+            edge_topic_assignments[a] = as<int>(get_token_topic_assignment(document,sampled_token));
+        }
+        
+    }
+    return edge_topic_assignments;
+}
