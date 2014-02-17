b08d102b888b36e218b37ee98c69e2e6806057f4
Matthew Denny <matthewjdenny@gmail.com>
2014-02-16 21:03:18 -0500

    Added several more functions and tested them. Also updates some data structures

--- TPME_R_Get_Wrapper_Functions.R
+++ TPME_R_Get_Wrapper_Functions.R
@@ -1,7 +1,10 @@
 #Function that wraps taking a log multinomial draw from a given distribution
 
 log_multinomial_draw <- function(probability_vector){
-    return(which(rmultinom(1,1,exp(probability_vector)) == 1))
+    #print(probability_vector)
+    edge_selected <-which(rmultinom(1,1,exp(probability_vector)) == 1)
+    #print(edge_selected)
+    return(edge_selected)
 }
 
 #log_multinomial_draw(rep(-.25,10))
@@ -20,7 +23,7 @@ get_token_topic_assignment <- function(document,token){
 #get_token_topic_assignment(1,1)
 
 get_sum_token_topic_assignments <- function(document,token,topic){
-    return(length(which(Token_Topic_Assignments[[document]] == topic)))
+    return(length(which(Token_Topic_Assignments[[document]][-token] == topic)))
 }
 
 #get_sum_token_topic_assignments(1,1,1)
@@ -28,3 +31,36 @@ get_sum_token_topic_assignments <- function(document,token,topic){
 get_edge_topic_assignment <- function(document,recipient){
     return(Edge_Topic_Assignments[document,recipient])
 }
+
+get_number_of_topics <- function(){
+    return(Number_Of_Topics)
+}
+
+#get_number_of_topics()
+
+get_number_of_unique_words <- function(){
+    return(Number_Of_Words)
+}
+
+#get_number_of_unique_words()
+
+get_word_type_topic_assignemnt_count <- function(document,token,topic){
+    temp <- Word_Type_Topic_Counts[Token_Word_Types[[document]][token],topic]
+    if(Token_Topic_Assignments[[document]][token] == topic){
+        temp <- temp -1
+    }
+    return(temp)
+}
+
+#get_word_type_topic_assignemnt_count(1,1,1)
+
+get_number_of_tokens_assigned_to_topic <- function(document,token,topic){
+    temp <- sum(Word_Type_Topic_Counts[,topic])
+    if(Token_Topic_Assignments[[document]][token] == topic){
+        temp <- temp -1
+    }
+    return(temp)
+}
+
+#get_number_of_tokens_assigned_to_topic(1,1,1)
+
