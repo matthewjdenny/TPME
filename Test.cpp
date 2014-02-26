e08adfe6f793462b7f01fcd403bed33b02574b31
Matthew Denny <matthewjdenny@gmail.com>
2014-02-26 13:52:53 -0500

    Added function to check the types of input for topic assingment step

--- Test.cpp
+++ Test.cpp
@@ -21,4 +21,40 @@ List test(NumericVector myArray){
   return(out); 
 
 }
+
+// [[Rcpp::export]]
+List test2(
+    int number_of_actors, 
+    int number_of_topics,
+    NumericVector tpec,
+    NumericVector taec,
+    NumericVector clp, 
+    NumericVector current_intercepts,
+    int number_of_latent_dimensions,
+    NumericMatrix betas,
+    int number_of_betas,
+    NumericVector indicator_array,
+    int number_of_itterations,
+    double proposal_variance,
+    int number_of_documents,
+    NumericMatrix observed_edges,
+    List token_topic_assignment_list,
+    List token_word_type_list,
+    NumericVector document_authors,
+double beta,
+NumericVector alpha_m,
+NumericMatrix edge_topic_assignments,
+NumericMatrix token_type_topic_counts,
+NumericVector topic_token_sums,
+int number_of_word_types
+    ){
   
+    List to_return(1);
+    to_return[0] = 2;
+    return to_return;
+    }  
+  
+    
+    
+    
+    
