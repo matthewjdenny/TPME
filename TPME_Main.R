a7c78a0d352cb121b2428db78ba48a4f8a0038ed
Matthew Denny <matthewjdenny@gmail.com>
2014-03-03 19:56:51 -0500

    Cleaned up file so it can be sourced from main script

--- TPME_Main.R
+++ TPME_Main.R
@@ -11,9 +11,20 @@ source("TPME_Run_Analysis.R")
 
 # 3. Load data: vocab file, document word matrix and document edge matrix
 
+#======== McDowell - 2011 =========#
+author_attributes =  read.csv("~/Dropbox/PINLab/Projects/Denny_Working_Directory/Remove_Names_2011/mcdowell/Email_Name_Dept_Gender_2011.csv", header= T, stringsAsFactors = F)
+#making a judgement call that Robbin silvers in a woman
+author_attributes[14,4] <- F
+document_edge_matrix = read.csv("~/Dropbox/PINLab/Projects/Denny_Working_Directory/Remove_Names_2011/mcdowell/edge-matrix.csv", header= F, stringsAsFactors = F)
+document_edge_matrix = document_edge_matrix[,-1]
+document_edge_matrix[,1] <- document_edge_matrix[,1] + 1 #make sure that authors are indexed starting at 1
+document_word_matrix = read.csv("~/Dropbox/PINLab/Projects/Denny_Working_Directory/Remove_Names_2011/mcdowell/word-matrix.csv", header= F, stringsAsFactors = F)
+document_word_matrix = document_word_matrix[,-1]
+vocabulary = read.csv("~/Dropbox/PINLab/Projects/Denny_Working_Directory/Remove_Names_2011/mcdowell/vocab.txt", header= F, stringsAsFactors = F)
+#===================================#
 
 # 4. Run analysis
-Result <- Run_Analysis( <- function(Number_Of_Iterations = 1000, Base_Alpha =1, Base_Beta = 0.01, Number_Of_Topics = 50, Author_Attributes, Document_Edge_Matrix ,Document_Word_Matrix, Vocabulary, Latent_Dimensions)
+Result <- Run_Analysis(Number_Of_Iterations = 50)
     
 
 # 5. Output and Analyze results
