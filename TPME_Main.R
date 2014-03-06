14940d00af62d5be31df8221018ded09b33ab3e8
Matthew Denny <matthewjdenny@gmail.com>
2014-03-05 20:22:25 -0500

    Added diagnostics functionality

--- TPME_Main.R
+++ TPME_Main.R
@@ -7,21 +7,25 @@ rm(list=ls())
 setwd("~/Dropbox/PINLab/Projects/R_Code/TPMNE")
 
 #requires RcppArmadillo and all dependenices be installed
-install.packages("RcppArmadillo")
+#install.packages("RcppArmadillo")
 
 # 2. Load user defined functions
 source("./Scripts/TPME_Run_Analysis.R")
+source("./Scripts/TPME_Model_Diagnostics.R")
 
 # 3. Load data: vocab file, document word matrix, document edge matrix and actor covariates
 
 # choose a dataset to work with:
-# load("./Data/McDowell_2011_Data.Rdata")
+ load("./Data/McDowell_2011_Data.Rdata")
 # load("./Data/New_Hannover_2011_Data.Rdata")
 # load("./Data/Transylvania_2011_Data.Rdata")
 # load("./Data/Columbus_2011_Data.Rdata")
 
 # 4. Run analysis for 50,000 itterations by setting equal to 50
-Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "Results_McDowell_3-4-14")
+Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 1,Run_Sample_Step = T,output_file = "Results_Test")
+
+# 5. Model Diagnostic plots 
+Generate_Model_Diagnsotics(output_file = "Current_Itteration_Results_McDowell_3-4-14",Actors = 17,Itterations = 1000, Latent_Spaces = 2, Topics = 50,LS_Actor = 2, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/")
     
 
 # 5. Output and Analyze results
