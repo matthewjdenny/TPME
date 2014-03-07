ac3415fe564653776ba9f30e21815256ddff1eb6
Matthew Denny <matthewjdenny@gmail.com>
2014-03-07 01:03:36 -0500

    Updated to run more diagnsotics

--- TPME_Main.R
+++ TPME_Main.R
@@ -16,16 +16,20 @@ source("./Scripts/TPME_Model_Diagnostics.R")
 # 3. Load data: vocab file, document word matrix, document edge matrix and actor covariates
 
 # choose a dataset to work with:
-#load("./Data/McDowell_2011_Data.Rdata")
-# load("./Data/New_Hannover_2011_Data.Rdata")
-load("./Data/Transylvania_2011_Data.Rdata")
-#load("./Data/Columbus_2011_Data.Rdata")
+load("./Data/McDowell_2011_Data.Rdata")
+#load("./Data/New_Hannover_2011_Data.Rdata")
+#load("./Data/Transylvania_2011_Data.Rdata")
+# load("./Data/Columbus_2011_Data.Rdata")
 
 # 4. Run analysis for 50,000 itterations by setting equal to 50
-Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "Transylvania_2011_3-5-14",Base_Alpha =1, Base_Beta = 0.01, Number_Of_Topics = 50,Proposal_Variance_Vector = c(.5,.1,.01,0.001))
+Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "New_Hannover_2011_3-6-14",Base_Alpha =1, Base_Beta = 0.01, Number_Of_Topics = 50,Proposal_Variance_Vector = c(.5,.1,.05,0.005))
 
 # 5. Model Diagnostic plots 
-#Generate_Model_Diagnsotics(output_file = "Current_Itteration_McDowell_2011_3-5-14",Actors = 17,Itterations = 1000, Latent_Spaces = 2, Topics = 50,LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/")
+#Generate_Model_Diagnsotics(input_folder_path = "./Output/",input_file = "Sample_Step_Columbus_2011_3-5-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "Columbus_County")
+
+#Generate_Model_Diagnsotics(input_folder_path = "./Output/",input_file = "Sample_Step_McDowell_2011_3-5-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "McDowell_County")
+
+Generate_Model_Diagnsotics(input_file = "Current_Itteration_McDowell_2011_3-6-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "McDowell_County_3-6-14", Thin_Itterations = 1)
     
 
 # 5. Output and Analyze results
