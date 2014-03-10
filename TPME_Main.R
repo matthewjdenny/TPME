3f40d4f73f4bedf03e4dd78be1b3bf19c70d2825
Matthew Denny <matthewjdenny@gmail.com>
2014-03-09 20:22:44 -0400

    updated to run more models

--- TPME_Main.R
+++ TPME_Main.R
@@ -12,19 +12,21 @@ setwd("~/Dropbox/PINLab/Projects/R_Code/TPMNE")
 # 2. Load user defined functions
 source("./Scripts/TPME_Run_Analysis.R")
 source("./Scripts/TPME_Model_Diagnostics.R")
+source("./Scripts/TPME_Take_Sample.R")
+
 
 # 3. Load data: vocab file, document word matrix, document edge matrix and actor covariates
 
 # choose a dataset to work with:
 #load("./Data/McDowell_2011_Data.Rdata")
 #load("./Data/New_Hannover_2011_Data.Rdata")
-#load("./Data/Transylvania_2011_Data.Rdata")
-load("./Data/Columbus_2011_Data.Rdata")
+load("./Data/Transylvania_2011_Data.Rdata")
+#load("./Data/Columbus_2011_Data.Rdata")
 
 # 4. Run analysis for 50,000 itterations by setting equal to 50
-Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "Columbus_2011_3-7-14",Base_Alpha =.1, Base_Beta = 0.01, Number_Of_Topics = 50,Proposal_Variance_Vector = c(.5,.1),post_burin_variance = 0.01)
+#Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "Columbus_2011_3-7-14",Base_Alpha =.1, Base_Beta = 0.01, Number_Of_Topics = 50,Proposal_Variance_Vector = c(.5,.1),post_burin_variance = 0.01)
 
-#Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "McDowell_2011_3-7-14",Base_Alpha =1, Base_Beta = 0.01, Number_Of_Topics = 50,Proposal_Variance_Vector = c(.5,.1),post_burin_variance = 0.001)
+#Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "McDowell_2011_3-8-14",Base_Alpha =1, Base_Beta = 0.01, Number_Of_Topics = 50,Proposal_Variance_Vector = c(.5,.1),post_burin_variance = 0.01,Metropolis_Step_Itterations = 1000)
 
 #Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,output_file = "Transylvania_2011_3-7-14",Base_Alpha =.1, Base_Beta = 0.01, Number_Of_Topics = 50,Proposal_Variance_Vector = c(.5,.1,.05),post_burin_variance = 0.005)
 
@@ -33,14 +35,24 @@ Model_Accept_Rate <- Run_Analysis(Number_Of_Iterations = 50,Run_Sample_Step = T,
 # 5. Model Diagnostic plots 
 #Generate_Model_Diagnsotics(input_folder_path = "./Output/",input_file = "Sample_Step_Columbus_2011_3-5-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "Columbus_County")
 
-#Generate_Model_Diagnsotics(input_folder_path = "./Output/",input_file = "Sample_Step_McDowell_2011_3-5-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "McDowell_County")
+#Generate_Model_Diagnsotics(input_file = "Sample_0.05_McDowell_2011_3-7-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "McDowell_County_0.05_3-7-14", Thin_Itterations = 1)
 
-#Generate_Model_Diagnsotics(input_file = "Current_Itteration_McDowell_2011_3-6-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "McDowell_County_3-6-14", Thin_Itterations = 1)
+#Generate_Model_Diagnsotics(input_file = "Sample_0.05_Columbus_2011_3-7-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "Columbus_County_0.05_3-7-14", Thin_Itterations = 1)
+
+#Generate_Model_Diagnsotics(input_file = "Sample_0.05_Transylvania_2011_3-7-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "Tramnsylvania_County_0.05_3-7-14", Thin_Itterations = 1)
 
 #Generate_Model_Diagnsotics(input_file = "Current_Itteration_New_Hannover_2011_3-6-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "New_Hannover_County_3-6-14", Thin_Itterations = 1)
 
-#Generate_Model_Diagnsotics(input_file = "Sample_Step_McDowell_2011_3-6-14",LS_Actor = 8, out_directory = "~/Dropbox/PINLab/Projects/Denny_Working_Directory/2011_Analysis_Output/", vocab = vocabulary,county_name = "McDowell_County_Sample_Step_3-6-14", Thin_Itterations = 1)
 
+#5 run additional sample steps:
+
+#Run_Sample_Step(input_file = "Current_Itteration_McDowell_2011_3-7-14",data_source = "McDowell_2011_Data", output_file = "Sample_Corrected_McDowell_2011_3-7-14", itterations = 1200000, proposal_variance = 0.05,sample_every = 100)
+
+#Run_Sample_Step(input_file = "Current_Itteration_Columbus_2011_3-7-14",data_source = "Columbus_2011_Data", output_file = "Sample_Corrected_Columbus_2011_3-7-14", itterations = 1200000, proposal_variance = 0.05,sample_every = 100)
+
+Run_Sample_Step(input_file = "Current_Itteration_Transylvania_2011_3-7-14",data_source = "Transylvania_2011_Data", output_file = "Sample_Corrected_Transylvania_2011_3-7-14", itterations = 1200000, proposal_variance = 0.05,sample_every = 100)
+
+ 
 
 
 # 5. Output and Analyze results
